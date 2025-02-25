272837c25aba442369ba4aa62c8d5afd615e749d
mhanans <marius.hanan@hq.accelist.com>
2025-02-25 16:23:56 +0700

    1 folder only

--- model_validator.py
+++ model_validator.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import AutoModelForCausalLM
 
-model = AutoModelForCausalLM.from_pretrained("./fine_tuned_model_merged")
+model = AutoModelForCausalLM.from_pretrained("./fine_tuned_model")
 for name, param in model.named_parameters():
     if torch.isnan(param).any() or torch.isinf(param).any():
         print(f"Parameter {name} contains nan or inf")
